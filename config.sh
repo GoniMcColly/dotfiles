fd9a3b1f269cf45dc36d26f723a692d4cd5f3d73
Goni McColly <agron.makolli@ost.ch>
2024-03-27 12:43:32 +0100

    update bash scripts

--- config.sh
+++ config.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
+if [ `id -u` -ne 0 ]
+  then echo Please run this script using sudo!
+  exit
+fi
+
+original_user=$(who am i | awk '{print $1}')
 config_DIR=$(pwd)
-echo $config_DIR
+home_DIR=$(/home/"$original_user")
 
+# home
+cd && cp -v "$config_DIR"/.bashrc "$home_DIR"/.bashrc
+cp -rv "$config_DIR"/wallpapers "$home_DIR"/Pictures/.
 
-cd && cp -v $config_DIR/.bashrc .bashrc
-cp -rv $config_DIR/wallpapers Pictures/.
+# .config 
+cp -rv "$config_DIR"/.config "$home_DIR"/.
 
+# /usr/share files
+cp -r "$config_DIR"/usr/share/regolith* /usr/share/. && echo "Regolith usr/share files copied" || return
+cp -r "$config_DIR"/usr/share/xfce4 /usr/share/. && echo "xfce4-terminal files copied" || return
 
-cp -rv $config_DIR/.config .
-
-cd /usr/share
-cp -rv $config_DIR/usr/share/regolith* . || echo "Copy manually: sudo cp -rv "$config_DIR"/usr/share/regolith* /usr/share/." && return
-cp -rv $config_DIR/usr/share/xfce4 . || return
-
-
-cd /etc 
-sudo cp -rv $config_DIR/etc/regolith . || echo "Copy etc file manually" && return
+#etc files
+cp -r "$config_DIR"/etc/regolith /etc/. && echo "regolith /etc files copied"  || echo "Copy etc file manually" && return
